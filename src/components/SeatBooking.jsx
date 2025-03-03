ca1b377c0874130fd1ee73ad59aa7b14c7056936
Daksh Pratap Singh <daksh6666singh@gmail.com>
2025-03-03 21:48:50 +0530

    Initial commit

--- src/components/SeatBooking.jsx
+++ src/components/SeatBooking.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+//Seat Booking frontend code
 const SeatBooking = () => {
   const [totalSeats, setTotalSeats] = useState(80);
   const [bookedSeats, setBookedSeats] = useState([]);
